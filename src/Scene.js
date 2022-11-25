6f189b0a0fde9794f489b5a504b45978a53f592c
Jo Blaszak <jblaszak@gmail.com>
2022-11-25 12:37:41 -0800

    Added basic portal w/ construction animation.

--- src/Scene.js
+++ src/Scene.js
@@ -1,5 +1,14 @@
+import Portal from "./Portal";
 import "./style.css";
 
 export default function Scene() {
-  return <></>;
+  return (
+    <>
+      <color attach="background" args={["#06092c"]} />
+      <pointLight position={[-20, 10, 25]} />
+      <ambientLight intensity={0.5} />
+      <Portal portalPosition={[0, 0.5, 0]} />
+      <gridHelper args={[50, 50, "#4D089A", "#4D089A"]} position={[0, 0, 0]} />
+    </>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import "./style.css";
 import { Canvas } from "@react-three/fiber";
 import * as THREE from "three";
 import Scene from "./Scene";
+import { OrbitControls } from "@react-three/drei";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Canvas camera={{ position: new THREE.Vector3(8, 5, 40) }}>
+  <Canvas camera={{ position: new THREE.Vector3(0, 5, 10) }}>
+    <OrbitControls makedefault />
     <Scene />
   </Canvas>
 );
